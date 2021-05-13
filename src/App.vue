c1e4b654d2a9d9e11e04d3a1bfa871115760ff2b
EtErNaLiFe12 <wnsqja8888@gmail.com>
2021-05-13 18:25:25 +0900

    vue3-component upload

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<!--component-1
+<template>
+  <MyBtn>Banana</MyBtn>
+  <MyBtn
+    :color="color">
+    <span style="color: red;">Banana</span>
+  </MyBtn>
+  <MyBtn
+    large
+    color="Orange">
+    Apple
+  </MyBtn>
+  <MyBtn
+    large
+    color="royalblue">
+    Grape
+  </MyBtn>
+</template>
+
+<script>
+import MyBtn from '~/components/MyBtn'
+
+export default {
+  components: {
+    MyBtn
+  },
+  data() {
+    return {
+      color: '#000'
+    }
+  }
+}
+</script>-->
+
+<!--속성 상속-->
+<template>
+  <MyBtn
+    class="heropy"
+    style="color: red;"
+    title="Hello world!">
+    Banana
+  </MyBtn>
+  <MyBtn class="jb">
+    apple
+  </MyBtn>
+</template>
+
+<script>
+import MyBtn from '~/components/MyBtn'
+
+export default {
+  components: {
+    MyBtn
+  }
+}
+</script>
---
+++ src/components/MyBtn.vue
@@ -0,0 +1,74 @@
+<!--MyBtn-1
+<template>
+  <div 
+    :class="{ large }"
+    :style="{ backgroundColor: color }"
+    class="btn">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+      color: {
+        type: String,
+        default: 'gray'
+    },
+    large: {
+      type: Boolean,
+      default: false
+    }
+    // text: {
+    //   type: String,
+    //   default: ''
+    // }
+  }
+}
+</script>
+<style scoped lang="scss">
+  .btn {
+      display: inline-block;
+      margin: 4px;
+      padding: 6px 12px;
+      border-radius: 4px;
+      background-color: gray;
+      color: white;
+      cursor: pointer;
+      &.large {
+      font-size: 20px;
+      padding: 10px 20px;
+    }
+  }
+  
+</style>-->
+
+<!--MyBtn-2-->
+<template>
+  <div class="btn">
+    <slot></slot>
+  </div>
+  <h1 v-bind="$attrs"></h1>
+  <div v-bind="$attrs"></div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  created() {
+    console.log(this.$attrs)
+  }
+}
+</script>
+
+<style scoped>
+  .btn {
+    display: inline-block;
+    margin: 4px;
+    padding: 6px 12px;
+    border-radius: 4px;
+    background-color: gray;
+    color: white;
+    cursor: pointer;
+  }
+</style>
